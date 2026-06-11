74dedd42a8e47eecf57af5c30aca5fcdafed4d1c
moyu1254 <moyukiti1254@gmail.com>
2026-06-11 11:33:08 +0900

    Minimize Windows distribution files

--- build_windows_exe.bat
+++ build_windows_exe.bat
@@ -13,17 +13,9 @@ python -m pip install --upgrade pip || goto :error
 python -m pip install pyinstaller || goto :error
 pyinstaller --clean --noconfirm "auto_url_fixer.spec" || goto :error
 
+if exist "dist\Auto URL Fixer" rmdir /S /Q "dist\Auto URL Fixer"
 if not exist "dist\Auto URL Fixer" mkdir "dist\Auto URL Fixer"
 copy /Y "dist\Auto URL Fixer.exe" "dist\Auto URL Fixer\Auto URL Fixer.exe" >nul || goto :error
-copy /Y "start_auto_url_fixer.bat" "dist\Auto URL Fixer\" >nul || goto :error
-copy /Y "start_auto_url_fixer.vbs" "dist\Auto URL Fixer\" >nul || goto :error
-copy /Y "stop_auto_url_fixer.bat" "dist\Auto URL Fixer\" >nul || goto :error
-copy /Y "stop_auto_url_fixer.vbs" "dist\Auto URL Fixer\" >nul || goto :error
-copy /Y "stop_auto_url_fixer.ps1" "dist\Auto URL Fixer\" >nul || goto :error
-copy /Y "enable_startup_auto_url_fixer.bat" "dist\Auto URL Fixer\" >nul || goto :error
-copy /Y "enable_startup_auto_url_fixer.vbs" "dist\Auto URL Fixer\" >nul || goto :error
-copy /Y "disable_startup_auto_url_fixer.bat" "dist\Auto URL Fixer\" >nul || goto :error
-copy /Y "disable_startup_auto_url_fixer.vbs" "dist\Auto URL Fixer\" >nul || goto :error
 copy /Y "config.example.json" "dist\Auto URL Fixer\" >nul || goto :error
 copy /Y "README.md" "dist\Auto URL Fixer\" >nul || goto :error
 
